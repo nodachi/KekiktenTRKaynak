6bd561e067755d45e5cd86f865456b7320db1a49
nodachi <75159683+nodachi@users.noreply.github.com>
2023-12-30 16:35:50 +0300

    d0.0.1
    dizigom ekleme çalışması ve dizipal güncellemesi

--- Dizigom/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
+++ Dizigom/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
@@ -14,8 +14,8 @@ import okhttp3.Response
 import org.jsoup.Jsoup
 
 class DiziPal : MainAPI() {
-    override var mainUrl              = "https://dizipal731.com"
-    override var name                 = "DiziPal"
+    override var mainUrl              = "https://www.dizigom1.com/"
+    override var name                 = "Dizigom"
     override val hasMainPage          = true
     override var lang                 = "tr"
     override val hasQuickSearch       = true
@@ -47,50 +47,23 @@ class DiziPal : MainAPI() {
     }
 
     override val mainPage = mainPageOf(
-        "${mainUrl}/diziler/son-bolumler"                          to "Son Bölümler",
-        "${mainUrl}/diziler"                                       to "Yeni Diziler",
-        "${mainUrl}/filmler"                                       to "Yeni Filmler",
-        "${mainUrl}/koleksiyon/netflix"                            to "Netflix",
-        "${mainUrl}/koleksiyon/exxen"                              to "Exxen",
-        "${mainUrl}/koleksiyon/blutv"                              to "BluTV",
-        "${mainUrl}/koleksiyon/disney"                             to "Disney+",
-        "${mainUrl}/koleksiyon/amazon-prime"                       to "Amazon Prime",
-        "${mainUrl}/koleksiyon/tod-bein"                           to "TOD (beIN)",
-        "${mainUrl}/koleksiyon/gain"                               to "Gain",
-        "${mainUrl}/tur/mubi"                                      to "Mubi",
-        "${mainUrl}/diziler?kelime=&durum=&tur=26&type=&siralama=" to "Anime",
-        "${mainUrl}/diziler?kelime=&durum=&tur=5&type=&siralama="  to "Bilimkurgu Dizileri",
-        "${mainUrl}/tur/bilimkurgu"                                to "Bilimkurgu Filmleri",
-        "${mainUrl}/diziler?kelime=&durum=&tur=11&type=&siralama=" to "Komedi Dizileri",
-        "${mainUrl}/tur/komedi"                                    to "Komedi Filmleri",
-        "${mainUrl}/diziler?kelime=&durum=&tur=4&type=&siralama="  to "Belgesel Dizileri",
-        "${mainUrl}/tur/belgesel"                                  to "Belgesel Filmleri",
-        "${mainUrl}/diziler?kelime=&durum=&tur=25&type=&siralama=" to "Erotik Diziler",
-        "${mainUrl}/tur/erotik"                                    to "Erotik Filmler",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=1&type=&siralama="  to "Aile",            // ! Fazla kategori olduğu için geç yükleniyor..
-        // "${mainUrl}/diziler?kelime=&durum=&tur=2&type=&siralama="  to "Aksiyon",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=3&type=&siralama="  to "Animasyon",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=4&type=&siralama="  to "Belgesel",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=6&type=&siralama="  to "Biyografi",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=7&type=&siralama="  to "Dram",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=8&type=&siralama="  to "Fantastik",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=9&type=&siralama="  to "Gerilim",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=10&type=&siralama=" to "Gizem",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=12&type=&siralama=" to "Korku",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=13&type=&siralama=" to "Macera",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=14&type=&siralama=" to "Müzik",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=16&type=&siralama=" to "Romantik",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=17&type=&siralama=" to "Savaş",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=24&type=&siralama=" to "Yerli",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=18&type=&siralama=" to "Spor",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=19&type=&siralama=" to "Suç",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=20&type=&siralama=" to "Tarih",
-        // "${mainUrl}/diziler?kelime=&durum=&tur=21&type=&siralama=" to "Western",
+        "${mainUrl}/tum-bolumler/"                          to "Son Bölümler",
+        "${mainUrl}/dizi-arsivi-hd1/"                                       to "Yeni Diziler",
+        "${mainUrl}/tum-yabanci-filmler-hd2/"                                       to "Yeni Filmler",
+        "${mainUrl}/netflix-dizileri-hd1/"                            to "Netflix",
+        "${mainUrl}/apple-tv-dizileri-hd1/"                              to "Apple TV+",
+        "${mainUrl}/kore-dizileri-hd1/"                             to "Kore Dizileri",
+        "${mainUrl}/japon-dizileri-hd1/"                       to "Japon Dizileri",
+		"${mainUrl}/cin-dizileri-hd1/"                              to "Çin Dizileri",
+        "${mainUrl}/marvel-dizileri-hd1/"                           to "Marvel Dizileri",
+        "${mainUrl}/dc-comics-dizileri-hd1/"                           to "DC Comics Dizileri",
+        "${mainUrl}/anime-dizileri-hd1/"                               to "Anime Diziler",
+        "${mainUrl}/efsane-diziler-hd1/" 							to "Efsane Diziler",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(request.data, interceptor = interceptor).document
-        val home     = if (request.data.contains("/diziler/son-bolumler")) {
+        val home     = if (request.data.contains("/tum-bolumler/")) {
             document.select("div.episode-item").mapNotNull { it.sonBolumler() } 
         } else {
             document.select("article.type2 ul li").mapNotNull { it.diziler() }
@@ -174,7 +147,7 @@ class DiziPal : MainAPI() {
         val rating      = document.selectXpath("//div[text()='IMDB Puanı']//following-sibling::div").text().trim().toRatingInt()
         val duration    = Regex("(\\d+)").find(document.selectXpath("//div[text()='Ortalama Süre']//following-sibling::div").text() ?: "")?.value?.toIntOrNull()
 
-        if (url.contains("/dizi/")) {
+        if (url.contains("/dizi-arsivi-hd1/")) {
             val title       = document.selectFirst("div.cover h5")?.text() ?: return null
 
             val episodes    = document.select("div.episode-item").mapNotNull {
